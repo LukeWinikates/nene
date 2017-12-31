5db6c6ca079e8b3658fcf92c1d41a9547d9d52b0
Luke Winikates <luke.winikates@gmail.com>
2017-12-30 22:52:15 -0800

    make the title look like the picture I drew (getting the text lined up properly is not very elegant)

--- src/nene/routes/home.clj
+++ src/nene/routes/home.clj
@@ -5,12 +5,11 @@
 (defn home []
   (layout/common
     [:header.banner
-     [:div.banner-block.title
-      [:h1.en "nene"]
-      [:h1.jp "音々"]]
-     [:div.banner-block.teaser
-      [:h3.en "Gitaigo & Giongo Explorer"]
-      [:h3.jp "ぎたいご・ぎおんご探険器具"]]]
+     [:h1.en {:style "grid-column: 1; grid-row:1; align-self:end"} "nene"]
+     [:h1.jp {:style "grid-column: 1; grid-row:2; align-self:start;"} "音々"]
+     [:h3.en {:style "grid-column: 2; grid-row:1; align-self:end"} "Gitaigo & Giongo Explorer"]
+     [:h3.jp {:style "grid-column: 2; grid-row:2; align-self:start"} "ぎたいご・ぎおんご探険器具"]]
+    ;]
     [:section.main
      [:script "Elm.Main.embed(document.querySelector(\".main\"))"]]))
 
