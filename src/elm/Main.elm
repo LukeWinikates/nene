fa732298e0f52c0d7cd2fa48d12dd6b3680dd099
Luke Winikates <luke.winikates@gmail.com>
2018-01-02 09:18:58 -0800

    show relationships on word detail page

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, article, button, div, em, header, section, span, str
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Http
+import Dict
 import Json.Decode as Decode exposing (field)
 
 
@@ -25,6 +26,7 @@ type alias Model =
     { notificationText : Maybe String
     , categories : Maybe (List Category)
     , page : Page
+    , relatives : Dict.Dict String (List Relative)
     }
 
 
@@ -33,33 +35,84 @@ emptyModel =
     { notificationText = Nothing
     , categories = Nothing
     , page = CategoryList
+    , relatives = Dict.empty
     }
 
 
 type Msg
     = CategoryFetch (Result Http.Error (List Category))
+    | RelativesFetch Word (Result Http.Error (List Relative))
     | PageChange Page
 
 
 update msg model =
-    ( case msg of
+    (case msg of
         CategoryFetch (Err er) ->
-            { model | notificationText = Just <| toString er }
+            { model | notificationText = Just <| toString er } |> noCommand
 
         CategoryFetch (Ok categories) ->
-            { model | notificationText = Nothing, categories = Just categories }
+            { model | notificationText = Nothing, categories = Just categories } |> noCommand
+
+        RelativesFetch _ (Err er) ->
+            { model | notificationText = Just <| toString er } |> noCommand
+
+        RelativesFetch word (Ok relatives) ->
+            (saveRelatives word relatives model) |> noCommand
 
         PageChange page ->
-            { model | page = page }
-    , Cmd.none
+            { model | page = page } |> pageCommand page
     )
+
+
+noCommand : Model -> ( Model, Cmd Msg )
+noCommand model =
+    ( model, Cmd.none )
+
+
+saveRelatives : Word -> List Relative -> Model -> Model
+saveRelatives word relatives model =
+    { model | relatives = Dict.insert word.romaji relatives model.relatives }
+
+
+pageCommand : Page -> Model -> ( Model, Cmd Msg )
+pageCommand page model =
+    ((,) model) <|
+        case page of
+            WordDetail word ->
+                Http.send (RelativesFetch word) (Http.get ("/api/gigo/" ++ word.romaji ++ "/relatives") relativesDecoder)
+
+            _ ->
+                Cmd.none
+
+
+type Loadable a
+    = NotLoaded
+    | Loaded a
+
+
+type alias Relative =
+    { label : String
+    , words : List String
+    }
 
 
 type alias Word =
     { kana : String
     , romaji : String
-    , attributes : List WordAttribute
+    , attributes :
+        List WordAttribute
+        --    , relatives : Loadable (List Relative)
     }
+
+
+mapLoadable : Loadable a -> (a -> b) -> Maybe b
+mapLoadable loadable f =
+    case loadable of
+        NotLoaded ->
+            Nothing
+
+        Loaded something ->
+            Just <| f something
 
 
 type alias WordAttribute =
@@ -99,6 +152,21 @@ wordDecoder =
         (field "attributes" (Decode.list attributeDecoder))
 
 
+
+--        (Decode.succeed NotLoaded)
+
+
+relativeDecoder : Decode.Decoder Relative
+relativeDecoder =
+    Decode.map2 Relative
+        (field "label" Decode.string)
+        (field "words" (Decode.list Decode.string))
+
+
+relativesDecoder =
+    field "relatives" (Decode.list relativeDecoder)
+
+
 wordView : Word -> Html Msg
 wordView word =
     div [ class "pill", onClick (PageChange <| WordDetail word) ]
@@ -119,8 +187,8 @@ categoryView category =
         ]
 
 
-wordDetailView : Word -> Html Msg
-wordDetailView word =
+wordDetailView : Word -> Maybe (List Relative) -> Html Msg
+wordDetailView word relatives =
     article [ class "super-rounded" ]
         [ header []
             [ strong [ style [ ( "font-size", "16px" ) ] ] [ text word.kana ]
@@ -138,6 +206,23 @@ wordDetailView word =
                 )
                 word.attributes
             )
+        , section []
+            ((Maybe.map
+                (\rels ->
+                    List.map
+                        (\relative ->
+                            div []
+                                [ strong [] [ text relative.label ]
+                                , span [] [ text ":" ]
+                                , (text <| String.join ", " relative.words)
+                                ]
+                        )
+                        rels
+                )
+                relatives
+             )
+                |> Maybe.withDefault []
+            )
         ]
 
 
@@ -149,7 +234,7 @@ pageView model =
                 |> Maybe.withDefault (text "no categories")
 
         WordDetail word ->
-            wordDetailView word
+            wordDetailView word (Dict.get word.romaji model.relatives)
 
 
 view model =
